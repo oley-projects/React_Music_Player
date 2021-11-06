2eaf62c726f4e821d1bb6f719e8146203adb18e9
oley-projects <oley.for.git@gmail.com>
2021-11-06 19:35:58 +0200

    useref, play/pause function

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,7 +1,19 @@
+import { useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay, faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
-const Player = () => {
+const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
+    //Ref
+    const audioRef = useRef(null);
+    //Handlers
+    const playSongHandler = () => {
+        if(isPlaying) {
+            audioRef.current.pause();
+        } else {
+            audioRef.current.play();
+        }
+        setIsPlaying(!isPlaying);
+    }
     return(
         <div className="player">
             <div className="time-control">
@@ -11,9 +23,10 @@ const Player = () => {
             </div>
             <div className="play-control">
                 <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
-                <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
+                <FontAwesomeIcon onClick={playSongHandler} className="play" size="2x" icon={faPlay} />
                 <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight} />
             </div>
+            <audio ref={audioRef} src={currentSong.audio}></audio>
         </div>
     );
 }
